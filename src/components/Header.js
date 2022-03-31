163159f5824f88b80b091664aea30566f74677ef
Jackie Tung <jackietung3@gmail.com>
2022-03-31 10:47:38 -0400

    react dom added

--- src/components/Header.js
+++ src/components/Header.js
@@ -2,6 +2,7 @@ import React,{useState, useEffect} from 'react';
 import Phase from './Phase'
 import Feedback from './Feedback';
 import FeedbackList from './FeedbackList';
+import {BrowserRouter as Router,Switch,Route,Link} from "react-router-dom";
 
 
 function Header({phases, feedback}){
@@ -24,35 +25,30 @@ function Header({phases, feedback}){
   console.log(currentFeedback)
   console.log(currentPhase)
   
-    return( 
-      <>
-    <header className="header sticky sticky--top js-header">
-
-  <div className="grid">
-
-  <nav className="navigation">
-      <ul className="navigation__list navigation__list--inline">
-        <li className="navigation__item"><a href="#" className="navigation__link navigation__link--is-active" onClick={homeButton}>Home</a></li>
-        <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="0">Pre-Work</a></li>
-        <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="1">Phase One</a></li>
-        <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="2">Phase Two</a></li>
-        <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="3">Phase Three</a></li>
-        <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="4">Phase Four</a></li>
-        <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="5">Phase Five</a></li>
-      </ul>
-    </nav>
-
-  </div>
-
-</header>
-
-<main className="main">
-
-  <div className="grid">
-    <Phase phase= {currentPhase} name={currentPhase.name} phaseName={currentPhase.Phase} description={currentPhase.description} show={show} videos={currentPhase.videos} currentFeedback={currentFeedback}/>
-  </div>
-
-</main>
+return( 
+  <>
+  <header className="header sticky sticky--top js-header">
+    <div className="grid">
+      <nav className="navigation">
+        <ul className="navigation__list navigation__list--inline">
+          <Router>
+          <li className="navigation__item"><a href="#" className="navigation__link navigation__link--is-active" onClick={homeButton}>Home</a></li>
+          <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="0">Pre-Work</a></li>
+          <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="1">Phase One</a></li>
+          <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="2">Phase Two</a></li>
+          <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="3">Phase Three</a></li>
+          <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="4">Phase Four</a></li>
+          <li className="navigation__item" ><a href="#" className="navigation__link" onClick={handlePhase} id="5">Phase Five</a></li>
+          </Router>
+        </ul>
+      </nav>
+    </div>
+  </header>
+  <main className="main">
+    <div className="grid">
+      <Phase phase= {currentPhase} name={currentPhase.name} phaseName={currentPhase.Phase} description={currentPhase.description} show={show} videos={currentPhase.videos} currentFeedback={currentFeedback}/>
+    </div>
+  </main>
 </>
 )
 }

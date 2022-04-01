8cd2ea6f0e329fbf5c8ed274b9e30168f7c2e3fb
Mario Brito <mariobrito94@hotmail.com>
2022-04-01 12:46:40 -0400

    about

--- src/components/AboutUs.js
+++ src/components/AboutUs.js
@@ -1,9 +1,13 @@
 function AboutUs (){
     return (
         <>
-    <h1>Mario And Jackie</h1>
-    <h3>Mario:</h3>
-    <h3>Jackie: Cookie and Cream is the best </h3>
+    <div align="center" font-size="large">Mario And Jackie</div>
+    <h2>Mario:</h2>
+    <img src="https://media-exp1.licdn.com/dms/image/C4D03AQFN_UpNKK-Eqw/profile-displayphoto-shrink_800_800/0/1533160390933?e=1654128000&v=beta&t=iJn9Zlgugo-qdCJ91e4SWsrBxGgGeJXfzvoWU605StE" class="MariosPic" />
+    <p align="center">Mario, is a Saiyan raised on Earth. Mario originally was sent to destroy Earth as an infant, there he would be adopted by Grandpa Mario who named him Mario. A head injury at an early age alters Mario's memory, ridding him of his initial destructive nature and allowing him to grow up to become one of Earth's greatest defenders. He constantly strives and trains to be the greatest warrior possible, which has kept the Earth and the universe safe from destruction many times.</p>
+    
+    <h2 align="center">Jackie: </h2>
+    <p align="center">Cookie and Cream is the best</p>
     <img src="https://cdn.geekwire.com/wp-content/uploads/2018/09/Flatiron-School_Classroom-1260x840.jpg" alt="img-FlatIron" className="ironpic" id="center"/>
     </>)
 }
